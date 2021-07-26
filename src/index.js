5e9889989f79df9061c7e1dfc9d4464df5083a27
kdrbek06 <kdrbek@gmail.com>
2021-07-26 23:27:29 +0200

    Fix source path error

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ document.getElementById("searchButton").addEventListener("click", () => {
     
     let inputCity = document.getElementById("inputBox").value;
     axios
-        .get(`http://api.openweathermap.org/data/2.5/forecast?q=${inputCity}&appid=${apiKey}&units=metric`)
+        .get(`https://api.openweathermap.org/data/2.5/forecast?q=${inputCity}&appid=${apiKey}&units=metric`)
         .then((response) => updateData(response.data))
         .catch((err) => console.log(err));
 });
--- src/js/updateData.js
+++ src/js/updateData.js
@@ -4,7 +4,7 @@ export const updateData = (data) => {
     console.log(data);
     let dayAvg = tempAvg(data);
 
-    document.getElementById("img0").src = `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
+    document.getElementById("img0").src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
     document.getElementById("cityName").innerHTML = data.city.name;
     document.getElementById("desc0").innerHTML = data.list[0].weather[0].description;
     document.getElementById("temp0").innerHTML = `Now:   ${Math.round(data.list[0].main.temp)}  °C`;
@@ -13,7 +13,7 @@ export const updateData = (data) => {
     document.getElementById("humidity0").innerHTML = `Humidity:  % ${Math.round(data.list[0].main.humidity)}`;
     
     for (let i = 1; i < 5; i++) {
-        document.getElementById(`img${i}`).src = `http://openweathermap.org/img/wn/${data.list[i * 8 - 1].weather[0].icon}@2x.png`;
+        document.getElementById(`img${i}`).src = `https://openweathermap.org/img/wn/${data.list[i * 8 - 1].weather[0].icon}@2x.png`;
         document.getElementById(`desc${i}`).innerHTML = data.list[i * 8 - 1].weather[0].description;
         document.getElementById(`avgBox${i}`).innerHTML = `Average:   ${dayAvg[i]}  °C`;}
         
